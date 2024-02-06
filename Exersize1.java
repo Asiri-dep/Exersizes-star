46ef297cfcf8468b1b375507fc6a7f5aec84564d
jayaneetha <jayaneethanamal@gmail.com>
2024-02-06 10:09:50 +0530

    Initial commit

---
+++ Exersize1.java
@@ -0,0 +1,13 @@
+void main(){
+    int x =0;
+
+    while(x++<5){
+        int y =x;
+        while(5-y++>=0){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+
+}
